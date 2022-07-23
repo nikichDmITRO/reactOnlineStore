01fb62a1c9de8fc1ff813e48e2b977ad3d2445c7
nikichDmITRO <dmitracenkonikita5@gmail.com>
2022-07-24 00:22:40 +0300

    store 3 мар- устранил ошибку с массивом

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Items from "./components/Items";
+import React from "react";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+constructor(props){
+  super(props)
+  this.state={
+    items:[
+      {id:1,
+      title:'book1'},
+      {id:2,
+      title:'book2'}
+    ]
+  }
+}
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items}/>
+        <Footer />
+        
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Item extends Component {
+  render() {
+    return (
+      <div>{this.props.item.title}</div>
+    )
+  }
+}
